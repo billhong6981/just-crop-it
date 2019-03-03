83239205fdb0bdf615bb52bf2bd84d71fa77aa69
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2019-03-02 23:22:23 -0800

    Format code and add logic to process only jpq and png

--- crop.py
+++ crop.py
@@ -3,25 +3,25 @@
 import os, requests, time
 
 # Prepare the folders
-originalFolder = 'original'
-croppedFolder = 'cropped'
+originalFolder = "original"
+croppedFolder = "cropped"
 
 if not os.path.exists(croppedFolder):
     os.makedirs(croppedFolder)
 
 # This is the URL
-url = 'http://croppola.com/croppola/image.jpg?aspectRatio=1:1&maximumHeight=90%&algorithm=croppola'
+url = "http://croppola.com/croppola/image.jpg?aspectRatio=1:1&maximumHeight=90%&algorithm=croppola"
 
 # Process all pictures
 for pictureFile in os.listdir(originalFolder):
-    print(pictureFile)
-    data = open(originalFolder + '/' + pictureFile, 'rb')
-    res = requests.post(url, data=data, headers={'User-Agent' : 'py'})
-    data.close()
-    if res.status_code == 200:
-        f = open(croppedFolder + '/' + pictureFile, 'wb')
-        f.write(res.content)
-        time.sleep(5)	# let other people crop
-    else:
-        print('Error ' + res.status_code)
-
+    if pictureFile.endswith((".jpg", ".png", ".JPG", ".PNG")):
+        print(pictureFile)
+        data = open(originalFolder + "/" + pictureFile, "rb")
+        res = requests.post(url, data=data, headers={"User-Agent": "py"})
+        data.close()
+        if res.status_code == 200:
+            f = open(croppedFolder + "/" + pictureFile, "wb")
+            f.write(res.content)
+            time.sleep(5)  # let other people crop
+        else:
+            print("Error " + res.status_code)
